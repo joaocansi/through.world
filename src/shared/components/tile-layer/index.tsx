2a8e6461f0fe593b3051e74afaf58a9dec3ad442
joaocansi <whitshs2003@gmail.com>
2023-08-27 18:29:33 -0300

    feat: mapbox style immplemented

--- src/shared/components/tile-layer/index.tsx
+++ src/shared/components/tile-layer/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable consistent-return */
 import { useMarker } from '@/shared/hooks/markers';
-// import { tileLayerHref } from '@/shared/utils/config/leaflet';
+import { tileLayerHref } from '@/shared/utils/config/leaflet';
 import { useEffect } from 'react';
 import { TileLayer as LeafletTileLayer, useMap } from 'react-leaflet';
 
@@ -21,13 +21,9 @@ export default function TileLayer() {
   }, [selectedMarker]);
 
   return (
-    // <LeafletTileLayer
-    //   attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    //   url={tileLayerHref}
-    // />`
     <LeafletTileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={tileLayerHref}
     />
   );
 }
