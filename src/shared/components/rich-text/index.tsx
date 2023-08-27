98c8341caa883ed602624af717f552bad4a08ec3
joaocansi <whitshs2003@gmail.com>
2023-08-27 20:48:57 -0300

    feat: contentful render options implemented to render images

--- src/shared/components/rich-text/index.tsx
+++ src/shared/components/rich-text/index.tsx
@@ -1,14 +1,32 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents, Options } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+
 import styles from './styles.module.scss';
 
 interface RichTextProps {
   document: any;
 }
 
+const renderOptions = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const { data } = node;
+      return (
+        <div className={styles.image}>
+          <img
+            src={`https://${data.target.fields.file.url}`}
+            alt={data.target.fields.description}
+          />
+        </div>
+      );
+    },
+  },
+} as Options;
+
 export default function RichText({ document }: RichTextProps) {
   return (
     <div className={styles.richtext}>
-      {documentToReactComponents(document)}
+      {documentToReactComponents(document, renderOptions)}
     </div>
   );
 }
