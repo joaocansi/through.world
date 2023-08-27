945d6950534a5d21af539573f3c72dcd02f41f52
joaocansi <whitshs2003@gmail.com>
2023-08-27 18:19:33 -0300

    feat: metadescription and metakeywords added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,9 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="shortcut icon" href="/earth-globe.png" type="image/x-icon" />
-          <meta name="keywords" content="trips,world,flights,eurotrip,my trips,about my trips,travel,travelling around the world" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Explore minhas aventuras ao redor do mundo enquanto compartilho minhas experiências de viagem por diversos países. Descubra culturas únicas, paisagens deslumbrantes e dicas valiosas para suas próprias jornadas." />
+          <meta name="keywords" content="viagens internacionais, países visitados, culturas globais, experiências de viagem, aventuras pelo mundo, dicas de viagem, destinos turísticos, explorando culturas, paisagens globais, itinerários de viagem" />
           <title>Through.World - Conheça as Minhas Viagens</title>
         </Head>
         <body>
