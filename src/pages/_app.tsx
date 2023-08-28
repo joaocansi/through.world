e2e3abd738cab3172a3b0684717509b11690e327
joaocansi <whitshs2003@gmail.com>
2023-08-27 22:15:48 -0300

    fix: no title or meta information in document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,19 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/900.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/earth-globe.png" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Explore minhas aventuras ao redor do mundo enquanto compartilho minhas experiências de viagem por diversos países. Descubra culturas únicas, paisagens deslumbrantes e dicas valiosas para suas próprias jornadas." />
+        <meta name="keywords" content="viagens internacionais, países visitados, culturas globais, experiências de viagem, aventuras pelo mundo, dicas de viagem, destinos turísticos, explorando culturas, paisagens globais, itinerários de viagem" />
+        <title>Through.World - Conheça as Minhas Viagens</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,11 @@
 import Document, {
-  Html, Head, NextScript, Main,
+  Html, NextScript, Main,
 } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head>
-          <link rel="shortcut icon" href="/earth-globe.png" type="image/x-icon" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="description" content="Explore minhas aventuras ao redor do mundo enquanto compartilho minhas experiências de viagem por diversos países. Descubra culturas únicas, paisagens deslumbrantes e dicas valiosas para suas próprias jornadas." />
-          <meta name="keywords" content="viagens internacionais, países visitados, culturas globais, experiências de viagem, aventuras pelo mundo, dicas de viagem, destinos turísticos, explorando culturas, paisagens globais, itinerários de viagem" />
-          <title>Through.World - Conheça as Minhas Viagens</title>
-        </Head>
         <body>
           <Main />
           <NextScript />
