c9466329ef0e85b0f3451003043ee1df1aae5542
joaocansi <whitshs2003@gmail.com>
2023-08-29 03:19:19 -0300

    fix: no head found in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
 import Document, {
-  Html, NextScript, Main,
+  Html, NextScript, Main, Head,
 } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
+        <Head />
         <body>
           <Main />
           <NextScript />
